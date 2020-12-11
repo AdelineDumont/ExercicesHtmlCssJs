1418ab9109217925bb4a1299e8d04ab68739901c
AdelineDumont <a.dumont@it-students.fr>
2020-12-11 16:46:14 +0100

    NewExos

--- script39.js
+++ script39.js
@@ -17,16 +17,16 @@ let NewNoeud3 = document.getElementById(PId);
 ParentDiv3.removeChild(NewNoeud3);
 }
 
-/*REMPLACER H1 DE LA DIV1 :
+
 ParentDiv1 = document.getElementById("Div1");
 let CreationH2 = document.createElement("h2");
 CreationH2.innerText = "Enfant numero 1";
-ParentDiv1.replaceChild(CreationH2,ParentDiv1.firstChild);*/
+ParentDiv1.replaceChild(CreationH2,ParentDiv1.firstChild);
 
 
 
-let ParentDuH1 = document.getElementById("LaDivDuH1");
+/*let ParentDuH1 = document.getElementById("LaDivDuH1");
 let CreationH2 = document.createElement("h2");
 CreationH2.innerText = "Le Titre h2";
-ParentDuH1.replaceChild(CreationH2,ParentDuH1.firstChild);
+ParentDuH1.replaceChild(CreationH2,ParentDuH1.firstChild);*/
 
